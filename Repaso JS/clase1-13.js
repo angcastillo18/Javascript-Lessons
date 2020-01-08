20bf3f6e927e62abbc1b04374511f68a021745e5
Angelo Castillo <angcastillo18@gmail.com>
2020-01-07 23:38:08 -0500

    repaso cap 1-13

---
+++ Repaso JS/clase1-13.js
@@ -0,0 +1,37 @@
+//Evita escribir mal,errores gramaticales
+/*
+Elimina algunos errores silenciosos de JavaScript cambiándolos a que lancen errores.
+Corrige errores que hacen difícil para los motores de JavaScript realizar optimizaciones: a veces, el código en modo estricto puede correr más rápido que un código idéntico pero no estricto.
+Prohibe cierta sintaxis que probablemente sean definidas en futuras versiones de ECMAScript.
+*/
+"use strict"
+//declarar variable
+//declaracion| inicializacion| utilizacion
+//variables constantes, varoles no modificables
+const PI=3.1416;
+//TIPOS DE DATOS
+//alert(PI)
+var Gato="no es un gato"
+//Tipos de datos:
+/*Numericos
+Cadena
+booleanos
+fechas(objeto)
+simbolos
+objetos*/
+var numero=12345;
+var cadena= "cadena";
+var estado=true;
+var fecha=new Date();
+var simbolo=Symbol();
+//Operadores aritmeticos
+// + - * / ; % modulo o resto
+// variable++ o -- postincremento (precedencia en operadores aritmeticos)=*/%+-
+// ++variable preincremento
+var uno=23;
+var dos=15;
+//uno++;
+//Operadores relacionales
+//<,>,<=,>=,==,!=, ===
+document.write(uno===dos);
+//Operadores logicos. Tiempo = 1:03:58
