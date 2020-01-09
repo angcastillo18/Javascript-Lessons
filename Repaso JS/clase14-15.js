a5271d7b6878b3984e276ea34909a672fbc75fd7
Angelo Castillo <angcastillo18@gmail.com>
2020-01-09 00:14:39 -0500

    add lessons 14-15

---
+++ Repaso JS/clase14-15.js
@@ -0,0 +1,40 @@
+var uno= 20;
+var dos=50;
+//CONDICIONALES
+/*
+if(uno==30){
+   document.write("Es igual");
+ }
+
+if(uno==30 || uno==""){
+   document.write("es igual")
+}else{
+   document.write("No es igual")
+}
+
+if(uno==29){
+   document.write("es igual a uno")
+}else if(dos==80){
+   document.write("es igual a dos")  
+}else{
+   document.write("no es igual a nada")
+}
+switch(uno){
+  case 20: document.write("igual") ; break;
+  case 30: document.write("diferente"); break;
+  default:; break;
+}
+*/
+//BUCLES, while dowhile for
+/*
+while(uno>=20){
+        document.write(uno + "<br>");
+        uno--;
+}
+
+do{
+        document.write(uno + "<br>");
+        uno--;
+}while(uno>=20);
+*/
+//ciclo for -- 2:00:00
